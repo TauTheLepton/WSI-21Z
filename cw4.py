b766143318fdda41a10d6d80f073b1548db69f36
TauTheLepton <matipalczuk@gmail.com>
2021-12-06 21:01:08 +0100

    tried some changes but none worked
    I have no idea why it is not working, for every param the model is calculated, equation is the same as on internet so should be good and I even checked mean and standard deviation values in libre office calc an they are calculated correctly as well

--- cw4.py
+++ cw4.py
@@ -13,25 +13,26 @@ def readFile(file_name):
 
 # was supposed to convert every element of data from string to float, but it doesn't matter
 def convertDataToFloat(data):
-    # print(type(data))
     for i in range(len(data)):
         for j in range(len(data[i])):
             data[i][j] = float(data[i][j])
     return data
 
-# devides data into learn data and test data with given coefficient
+# divides data into learn data and test data with given coefficient
 def divideLearnTestData(data, coef):
     learn_data = data[:int(len(data)*coef)]
     test_data = data[int(len(data)*coef):]
     return learn_data, test_data
 
+# divides data into given amount (k) of sets
 def divideDataIntoSets(data, k):
     sets = []
-    set_length = int(len(data) / k)
+    set_length = math.trunc(len(data) / k)
     for i in range(k):
         sets.append(data[i*set_length:(i+1)*set_length])
     return sets
 
+# merges all sets form list 'sets' without set 'exclude_set' into one set
 def mergeSets(sets, exclude_set):
     merged = []
     for set in sets:
@@ -64,13 +65,27 @@ def calculateOneModel(data, idx, d):
     for item in data:
         if item[d_idx] == d:
             d_data.append(item[idx])
+
+    # WRITE TO FILE TO CHECK WITH LIBRE OFFICE CALC IF STANDARD DEVIATION AND MEAN VALUES ARE CORRECT
+    # YES THEY ARE
+    # print()
+    # for item in d_data: print(item)
+    # print()
+    # with open('test_write_one_column.csv', mode='w') as file:
+    #     writer = csv.writer(file, delimiter='\n')
+    #     for item in d_data:
+    #         writer.writerow([item])
+
+    # if d_data != []:
     mean = np.mean(d_data)
     std = np.std(d_data)
+    # else:
+    #     mean = 0
+    #     std = 0
     return mean, std
 
 # calculates models od every idx for every d, so all models
-def calculateModels(data):
-    D = getDList(data)
+def calculateModels(data, D):
     models = {}
     for d in D:
         models_param = {}
@@ -87,16 +102,31 @@ def calculateModelValue(models, d, indiv):
     end_probab1 = 0
     *indiv_data, indiv_d = indiv
     for i in range(len(indiv_data)):
-        index = -(((indiv_data[i] - models[d][i]['mean']) ** 2) / (2 * models[d][i]['std'] ** 2))
-        probab = (1 / (math.sqrt(2 * math.pi) * models[d][i]['std'])) * math.exp(index)
+        # MY OLD CALCULATIONS, BUT RESULTS ARE PRETTY MUCH THE SAME
+        # if True:
+        # if models[d][i]['std'] ** 2 != 0:
+        #     index = -(((indiv_data[i] - models[d][i]['mean']) ** 2) / (2 * (models[d][i]['std'] ** 2)))
+        #     probab = (1 / (math.sqrt(2 * math.pi) * abs(models[d][i]['std']))) * math.exp(index)
+        # else:
+        #     probab = 0
+
+        # EQUATION STRAIGHT FROM https://towardsdatascience.com/how-to-impliment-a-gaussian-naive-bayes-classifier-in-python-from-scratch-11e0b80faf5a
+        # STANDARD DEVIATION STILL SOMETIMES 0
+        if models[d][i]['std'] ** 2 != 0:
+            exponent = math.exp(-((indiv_data[i] - models[d][i]['mean']) ** 2 / (2 * models[d][i]['std'] ** 2)))
+            probab = (1 / (math.sqrt(1 * math.pi) * models[d][i]['std'])) * exponent
+        else:
+            probab = 0
+        # if models[d][i]['std'] ** 2 == 0:
+        #     print(index, probab)
         end_probab *= probab
-        end_probab1 += math.log(probab, math.e)
+        if probab != 0:
+            end_probab1 += math.log(probab, math.e)
     end_probab1 = math.exp(end_probab1)
     return end_probab1
 
 # calculates best d for given item
-def calcBestD(data, models, test_item):
-    D = getDList(data)
+def calcBestD(D, models, test_item):
     best_probab = 0
     best_d = None
     for d in D:
@@ -110,14 +140,15 @@ def calcBestD(data, models, test_item):
 def test(file_name, coef):
     data, header = readFile(file_name)
     data = convertDataToFloat(data)
+    D = getDList(data)
     d_idx = getDIdx(data)
     learn_data, test_data = divideLearnTestData(data, coef)
-    models = calculateModels(learn_data)
+    models = calculateModels(learn_data, D)
     sum_best_d = 0
     count_best_d = 0
     win_rate = 0
     for test_item in test_data:
-        best_d = calcBestD(data, models, test_item)
+        best_d = calcBestD(D, models, test_item)
         if best_d == test_item[d_idx]:
             win_rate += 1
         else:
@@ -126,9 +157,11 @@ def test(file_name, coef):
     print("Win rate:", win_rate/len(test_data))
     print("Average mistake:", sum_best_d/count_best_d)
 
+# tests whole implementation with cross validation with given aamount of sets (k)
 def testCrossValidation(file_name, k):
     data, header = readFile(file_name)
     data = convertDataToFloat(data)
+    D = getDList(data)
     d_idx = getDIdx(data)
     dataSets = divideDataIntoSets(data, k)
     accuracy = []
@@ -136,12 +169,12 @@ def testCrossValidation(file_name, k):
     for dataSet in dataSets:
         learn_data = mergeSets(dataSets, dataSet)
         test_data = dataSet
-        models = calculateModels(learn_data)
+        models = calculateModels(learn_data, D)
         sum_best_d = 0
         count_best_d = 0
         win_rate = 0
         for test_item in test_data:
-            best_d = calcBestD(data, models, test_item)
+            best_d = calcBestD(D, models, test_item)
             if best_d == test_item[d_idx]:
                 win_rate += 1
             else:
@@ -155,14 +188,20 @@ def testCrossValidation(file_name, k):
     print("Average mistake:", end_mistake)
 
 def main():
-    # test('winequality-white.csv', 0.6)
-    testCrossValidation('winequality-white.csv', 4)
-    # data, header = readFile('winequality-white-test.csv')
-    # sets = divideDataIntoSets(data, 4)
-    # for set in sets:
-    #     print(set)
-    #     print()
-    # mergeSets(sets, sets[0])
+    red = 'winequality-red.csv'
+    white = 'winequality-white.csv'
+    white_test = 'winequality-white-test.csv'
+    my_set = white
+    print("60/40")
+    test(my_set, 0.6)
+    print("Cross validation")
+    testCrossValidation(my_set, 5)
+
+    # data, header = readFile(white)
+    # data = convertDataToFloat(data)
+    # mean, std = calculateOneModel(data, 1, 6)
+    # print('mean', mean)
+    # print('std', std)
 
 if __name__ == '__main__':
     main()
